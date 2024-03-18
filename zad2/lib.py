7853021c7f8fb9348c0a4f1bb580a9c0df1ddf0e
szjaniak1 <szjaniak1@gmail.com>
2024-03-18 18:48:46 +0100

    polynomials division operator works

--- zad2/lib.py
+++ zad2/lib.py
@@ -9,6 +9,10 @@ class polynomial():
     def coeffs(self) -> List[int]:
         return self._coeffs
 
+    @property
+    def deg(self) -> int:
+        return len(self.coeffs)
+
     def __str__(self) -> str:
         return str(self.coeffs)
     
@@ -22,7 +26,6 @@ class polynomial():
 
         return polynomial(coeffs_n)
 
-
     def __sub__(self, other) -> 'polynomial':
         if len(self.coeffs) < len(other.coeffs):
             return other + self
@@ -32,3 +35,17 @@ class polynomial():
            coeffs_n[i] = coeffs_n[i] - other.coeffs[i]
 
         return polynomial(coeffs_n)
+
+    def __truediv__(self, other) -> tuple['polynomial', 'polynomial']:
+        q = [0] * (self.deg - other.deg + 1) 
+        r = deepcopy(self.coeffs)
+
+        while len(r) >= other.deg:
+            curr_expo = len(r) - other.deg
+            lead_coeff = r[-1] / other.coeffs[-1]
+            q[curr_expo] = lead_coeff
+            for i in range(other.deg):
+                r[curr_expo + i] = r[curr_expo + i] - lead_coeff * other.coeffs[i]
+            r.pop()
+
+        return polynomial(q), polynomial(r)
--- zad2/main.py
+++ zad2/main.py
@@ -3,10 +3,13 @@ import sys
 from lib import *
 
 def main() -> int:
-    d = polynomial([1, 2, 3])
-    z = polynomial([3, 4])
+    d = polynomial([6, 4, 2])
+    z = polynomial([3, 2])
     print(d + z)
-    print(d - z)    
+    print(d - z)
+    q, r = d / z
+    print(q)
+    print(r)
     return 0
 
 if __name__ == '__main__':
