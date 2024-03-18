4625c7a8f4952f9bac2054512917409a8c0b86ab
szjaniak1 <szjaniak1@gmail.com>
2024-03-18 19:08:48 +0100

    gcd and lcm for polynomials work

--- zad2/lib.py
+++ zad2/lib.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
 from typing import List
 
+import numpy
+
 class polynomial():
     def __init__(self, coeffs: List[int]) -> None:
         self._coeffs = coeffs
@@ -32,9 +34,17 @@ class polynomial():
 
         coeffs_n = deepcopy(self.coeffs) 
         for i in range(len(other.coeffs)):
-           coeffs_n[i] = coeffs_n[i] - other.coeffs[i]
+           coeffs_n[i] -= other.coeffs[i]
 
         return polynomial(coeffs_n)
+
+    def __mul__(self, other) -> 'polynomial':
+        prod = [0] * (self.deg + other.deg - 1)
+        for i in range(self.deg):
+            for j in range(other.deg):
+                prod[i + j] += self.coeffs[i] * other.coeffs[j]
+
+        return polynomial(prod)
 
     def __truediv__(self, other) -> tuple['polynomial', 'polynomial']:
         q = [0] * (self.deg - other.deg + 1) 
@@ -45,7 +55,21 @@ class polynomial():
             lead_coeff = r[-1] / other.coeffs[-1]
             q[curr_expo] = lead_coeff
             for i in range(other.deg):
-                r[curr_expo + i] = r[curr_expo + i] - lead_coeff * other.coeffs[i]
+                r[curr_expo + i] -= lead_coeff * other.coeffs[i]
             r.pop()
 
         return polynomial(q), polynomial(r)
+
+
+def gcd(x: polynomial, y: polynomial) -> polynomial:
+    if x.deg < y.deg:
+        return gcd(y, x)
+    if y.deg == 1:
+        return x
+    _, r = x / y
+    return gcd(y, r)
+
+def lcm(x: polynomial, y: polynomial) -> polynomial:
+    gcd_ = gcd(x, y)
+    mul = x * y
+    return (mul / gcd_)[0]
--- zad2/main.py
+++ zad2/main.py
@@ -3,13 +3,15 @@ import sys
 from lib import *
 
 def main() -> int:
-    d = polynomial([6, 4, 2])
+    d = polynomial([6, 4])
     z = polynomial([3, 2])
     print(d + z)
     print(d - z)
     q, r = d / z
     print(q)
     print(r)
+    print(gcd(d, z))
+    print(lcm(d, z))
     return 0
 
 if __name__ == '__main__':
