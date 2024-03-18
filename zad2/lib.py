2245decf92cd2c8bebf73271da3c56aecebebc98
szjaniak1 <szjaniak1@gmail.com>
2024-03-18 18:21:51 +0100

    polynomials addition and subtraction operatiors works

--- zad2/lib.py
+++ zad2/lib.py
@@ -1,4 +1,34 @@
-class polynomial():
-    def __init__(self) -> None:
-        print("hello")
+from copy import deepcopy
+from typing import List
 
+class polynomial():
+    def __init__(self, coeffs: List[int]) -> None:
+        self._coeffs = coeffs
+
+    @property
+    def coeffs(self) -> List[int]:
+        return self._coeffs
+
+    def __str__(self) -> str:
+        return str(self.coeffs)
+    
+    def __add__(self, other) -> 'polynomial':
+        if len(self.coeffs) < len(other.coeffs):
+            return other + self
+
+        coeffs_n = deepcopy(self.coeffs) 
+        for i in range(len(other.coeffs)):
+           coeffs_n[i] = coeffs_n[i] + other.coeffs[i]
+
+        return polynomial(coeffs_n)
+
+
+    def __sub__(self, other) -> 'polynomial':
+        if len(self.coeffs) < len(other.coeffs):
+            return other + self
+
+        coeffs_n = deepcopy(self.coeffs) 
+        for i in range(len(other.coeffs)):
+           coeffs_n[i] = coeffs_n[i] - other.coeffs[i]
+
+        return polynomial(coeffs_n)
--- zad2/main.py
+++ zad2/main.py
@@ -3,7 +3,10 @@ import sys
 from lib import *
 
 def main() -> int:
-    d = polynomial()
+    d = polynomial([1, 2, 3])
+    z = polynomial([3, 4])
+    print(d + z)
+    print(d - z)    
     return 0
 
 if __name__ == '__main__':
