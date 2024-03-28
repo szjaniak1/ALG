3afe8c6bfe248abf6f0232098d62cbe0fc6f2cba
szjaniak <szjaniak1@gmail.com>
2024-03-28 23:48:16 +0100

    evaluation working, generating points improved

--- zad6/main.py
+++ zad6/main.py
@@ -9,25 +9,43 @@ class set():
         self.border_func = border_func
         self.value_func = value_func
 
+    def evaluate(self, point1: List[int], point2: List[int]) -> bool:
+        for (x, y) in zip(point1, point2):
+            if x > y:
+                return False
+        return True
+        
+    def gen_points(self, size: int, low_border: int, high_border: int) -> List:
+        res = []
+        if size > (high_border - low_border)**2:
+            return res
+        res_size = 0
+        while res_size < size:
+            p = []
+            for _ in range(self.expo):
+                p.append(randrange(low_border, high_border))
+            if p not in res and self.border_func(self.value_func(p)):
+                res.append(p)
+                res_size += 1
+
+        return res
+
 def find_minimal(s: set, points: List[List[int]]) -> List[List[int]]:
-    minimal = [[]]
+    minimal = []
+    flag = False
     for a in points:
-        print("000")
+        flag = True
+        for m in minimal:
+            if s.evaluate(a, m):
+                points.remove(a)
+                flag = False
+                break
+            minimal.remove(m)
+        if flag:
+            minimal.append(a)
+            points.remove(a)
+        
     return minimal
-
-def evaluate(point1: List[int], point2: List[int]) -> bool:
-    return False    
-
-def gen_points(k: int, size: int, low_border: int, high_border: int) -> List:
-    res = []
-    for _ in range(size):
-        p = []
-        for _ in range(k):
-            p.append(randrange(low_border, high_border))
-        if p not in res:
-            res.append(p)
-
-    return res
 
 def func1(point: List[int]) -> int:
     return point[0] * point[1]
@@ -35,17 +53,18 @@ def func1(point: List[int]) -> int:
 def func2(point: List[int]) -> int:
     return ((point[0] - 10)**2 + (point[1] - 10)**2)
 
-def border(value: int) -> tuple[bool, int]:
-    return value >= 11, abs(value - 11)
+def border(value: int) -> bool:
+    return value >= 11
 
-def border2(value: int) -> tuple[bool, int]:
-    return value <= 25, abs(value - 25)
+def border2(value: int) -> bool:
+    return value <= 25
 
 def main() -> int:
-    points = gen_points(10, 3, 0, 20)
-    #s = set(2, func1, border)
-    #minimal = find_minimal(s, points)
+    s = set(2, func1, border)
+    points = s.gen_points(40, 0, 11)
     print(points)
+    minimal = find_minimal(s, points)
+    print(minimal)
 
     return 0
 
